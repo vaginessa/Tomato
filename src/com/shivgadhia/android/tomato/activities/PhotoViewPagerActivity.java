42282ecda2e487c78bbc7cc14c3d80ad56e58604
shivgadhia <shivam.gadhia@googlemail.com>
2013-05-12 07:56:25 +0100

    Removing commented code

--- src/com/shivgadhia/android/tomato/activities/PhotoViewPagerActivity.java
+++ src/com/shivgadhia/android/tomato/activities/PhotoViewPagerActivity.java
@@ -32,10 +32,6 @@ public class PhotoViewPagerActivity extends Activity implements LoaderManager.Lo
         mViewPager = new HackyViewPager(this);
         setContentView(mViewPager);
         imageTagFactory = new ImageTagFactory(this, R.drawable.ic_launcher);
-//        ArrayList<ImageModel> imageModels = new ArrayList<ImageModel>();
-//        imageModels.add(new ImageModel("http://24.media.tumblr.com/tumblr_m98uytdr3Z1rxiqm6o1_1280.jpg", "http://25.media.tumblr.com/e7ad7269fdda256c800a21b4e4839601/tumblr_ml7jr4UbeX1qflpkjo1_1280.jpg", "title"));
-//        imageModels.add(new ImageModel("http://25.media.tumblr.com/e7ad7269fdda256c800a21b4e4839601/tumblr_ml7jr4UbeX1qflpkjo1_1280.jpg", "http://25.media.tumblr.com/e7ad7269fdda256c800a21b4e4839601/tumblr_ml7jr4UbeX1qflpkjo1_1280.jpg", "title"));
-//        imageModels.add(new ImageModel("http://25.media.tumblr.com/tumblr_m6hpirkbhE1r674r7o1_500.jpg", "http://25.media.tumblr.com/tumblr_m6hpirkbhE1r674r7o1_500.jpg", "title"));
         initLoader();
     }
 
